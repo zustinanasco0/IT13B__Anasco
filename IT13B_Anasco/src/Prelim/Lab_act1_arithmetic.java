0bc267ebdf21078f8caeae060fe4142482a2c50a
IT13B_Mentopa <mentopakimjulian@gmail.com>
2025-02-13 12:14:30 +0800

    Added Java Code

---
+++ IT13B_Anasco/src/Prelim/Lab_act1_arithmetic.java
@@ -0,0 +1,20 @@
+
+package Prelim;
+
+
+public class Lab_act1_arithmetic {
+      
+     public static void main (String[] args){
+        
+       int x = 10;
+       int y = 4;
+       int z = 6;
+       
+       System.out.println(x + "*" + y + " + " + z + " = "+ (x * y + z));
+       System.out.println(x + "-" + y + " % " + z + " = "+ ((x - y) % z)); 
+       System.out.println(x + "+" + y + " + " + z + " = "+ ((x + y + z) / 3));
+       System.out.println(x + "*" + z + " - " + y + " * " + y + " = " + (x * z - (y*y)));
+             
+    }
+}
+
